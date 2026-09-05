ed0053fc266af76cd6b836c2e431eb6e5f61d760
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-09-05 13:47:59 +0530

    Remove 'Func<TArgs, TResult>' support.

--- src/Flarial.Runtime/Services/TaskService.cs
+++ src/Flarial.Runtime/Services/TaskService.cs
@@ -5,7 +5,6 @@ namespace Flarial.Runtime.Services;
 static class TaskService
 {
     internal delegate void TaskAction<TArgs>(in TArgs args);
-    internal delegate TResult TaskFunc<TArgs, TResult>(in TArgs args);
 
     sealed class TaskState<TArgs>
     {
@@ -21,20 +20,6 @@ static class TaskService
         internal void Invoke() => _action(_args);
     }
 
-    sealed class TaskState<TArgs, TResult>
-    {
-        readonly TArgs _args;
-        readonly TaskFunc<TArgs, TResult> _function;
-
-        internal TaskState(TaskFunc<TArgs, TResult> function, in TArgs args)
-        {
-            _args = args;
-            _function = function;
-        }
-
-        internal TResult Invoke() => _function(_args);
-    }
-
     extension(Task)
     {
         internal static Task Run<TArgs>(TaskAction<TArgs> action, in TArgs args)
@@ -47,18 +32,6 @@ static class TaskService
             }
 
             return Task.Factory.StartNew(Action, state, default, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
-        }
-
-        internal static Task<TResult> Run<TArgs, TResult>(TaskFunc<TArgs, TResult> function, in TArgs args)
-        {
-            TaskState<TArgs, TResult> state = new(function, args);
-
-            static TResult Func(object? state)
-            {
-                return ((TaskState<TArgs, TResult>)state!).Invoke();
-            }
-
-            return Task.Factory.StartNew(Func, state, default, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
         }
     }
 }
